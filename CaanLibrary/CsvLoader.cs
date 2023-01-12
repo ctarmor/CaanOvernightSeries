c5e50d61f527e750d072e666c71b544bce56fb51
carlos <comtechy@outlook.com>
2023-01-11 21:56:13 -0500

    Add parsers

--- CaanLibrary/CsvLoader.cs
+++ CaanLibrary/CsvLoader.cs
@@ -3,14 +3,27 @@ using System.Text.Json;
 public class CsvLoader
 {
     /// <summary>
+    /// Parse all prices
+    /// </summary>
+    static public IEnumerable<DailyClose> ParseLines(IEnumerable<string> lines)
+    {
+        return lines
+            .Skip(1) // Skip header
+            .Select(x => ParseLine(x));
+    }
+
+    /// <summary>
     /// Paser a line imported from a file into.
     /// </summary>
     static public DailyClose ParseLine(string line)
     {
         // Date,Open,High,Low,Close
-        //  ["01/10/2023","3,888.57","3,919.83","3,877.29","3,919.25"]
+        // 01/10/2023,"3,888.57","3,919.83","3,877.29","3,919.25"
+        var cleanLine = line
+            .Replace(@",""", "^")
+            .Replace(@"""", "");
 
-        var fields = line.Split('^');
+        var fields = cleanLine.Split('^');
 
         return new DailyClose
         {
--- CaanLibraryTests/CsvParserTests.cs
+++ CaanLibraryTests/CsvParserTests.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using CaanLibrary;
 
 namespace CaanLibraryTests;
@@ -6,13 +7,40 @@ namespace CaanLibraryTests;
 public class CsvParserTests
 {
     [TestMethod]
+    public void TestParseLines()
+    {
+        var lines = new List<string> {
+            "Date,Open,High,Low,Close",
+            "01/10/2023,\"3,888.57\",\"3,919.83\",\"3,877.29\",\"3,919.25\"",
+            "01/10/2023,\"3,888.57\",\"3,919.83\",\"3,877.29\",\"3,919.25\""
+        };
+
+        var target = CsvLoader.ParseLines(lines);
+
+        var result = new List<DailyClose> {
+            new DailyClose {
+                DateStamp = DateTime.Parse("01/10/2023"),
+                Open = 3888.57M,
+                High = 3919.83M,
+                Low = 3877.29M,
+                Close = 3919.25M
+            },
+            new DailyClose {
+                DateStamp = DateTime.Parse("01/10/2023"),
+                Open = 3888.57M,
+                High = 3919.83M,
+                Low = 3877.29M,
+                Close = 3919.25M
+            }
+        };
+
+        Assert.IsTrue(target.SequenceEqual(result));
+    }
+
+    [TestMethod]
     public void TestParseLine()
     {
-        var cleanLine = "01/10/2023,\"3,888.57\",\"3,919.83\",\"3,877.29\",\"3,919.25\""
-            .Replace(@",""", "^")
-            .Replace(@"""", "");
-
-        var target = CsvLoader.ParseLine(cleanLine);
+        var target = CsvLoader.ParseLine("01/10/2023,\"3,888.57\",\"3,919.83\",\"3,877.29\",\"3,919.25\"");
 
         var result = new DailyClose
         {
